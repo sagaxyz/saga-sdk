9c4989c1050ec04a9c4bc63a13353385f25db542
Facundo <facundomedica@gmail.com>
2025-08-05 15:57:04 +0200

    replace receiver with known signer

--- x/transferrouter/middlewares/ibc.go
+++ x/transferrouter/middlewares/ibc.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"encoding/json"
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -9,6 +10,7 @@ import (
 	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
 	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
 	"github.com/cosmos/ibc-go/v8/modules/core/exported"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/sagaxyz/saga-sdk/x/transferrouter/keeper"
 	"github.com/sagaxyz/saga-sdk/x/transferrouter/types"
 )
@@ -69,6 +71,40 @@ func (i IBCMiddleware) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet,
 	var data transfertypes.FungibleTokenPacketData
 	if err := transfertypes.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
 		logger.Debug(fmt.Sprintf("OnRecvPacket payload is not a FungibleTokenPacketData: %s", err.Error()))
+		return i.app.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	// If it's a PFM packet meant to be forwarded, we return early
+	d := make(map[string]interface{})
+	err := json.Unmarshal([]byte(data.Memo), &d)
+	if err == nil && d["forward"] != nil {
+		// a packet meant to be forwarded, let the PFM module handle it
+		return i.app.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	// Move tokens to an escrow account by replacing the destination address in the packet data
+	params, err := i.k.Params.Get(ctx)
+	if err != nil {
+		i.k.Logger(ctx).Error("failed to get params", "error", err)
+		return i.app.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	// Parse the configured private key (in hex format) and derive the corresponding
+	// Ethereum address of the known signer.
+	privKey, err := crypto.HexToECDSA(params.KnownSignerPrivateKey)
+	if err != nil {
+		i.k.Logger(ctx).Error("failed to parse known signer private key", "error", err)
+		return i.app.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	knownSignerAddress := crypto.PubkeyToAddress(privKey.PublicKey)
+	newRecAddr := sdk.AccAddress(knownSignerAddress.Bytes())
+	data.Receiver = newRecAddr.String()
+
+	// update the packet data
+	packet.Data, err = json.Marshal(data)
+	if err != nil {
+		i.k.Logger(ctx).Error("failed to marshal packet data", "error", err)
 		return i.app.OnRecvPacket(ctx, packet, relayer)
 	}
 
